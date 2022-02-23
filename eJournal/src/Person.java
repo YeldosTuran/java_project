415fd0b7f54f9d8e5ad399344b63287ee1edf492
YeldosTuran <96955055+YeldosTuran@users.noreply.github.com>
2022-02-23 16:38:24 +0600

    Add files via upload

---
+++ eJournal/src/Person.java
@@ -0,0 +1,12 @@
+public interface Person {
+    int year = 0;
+    String name = "", surname = "";
+
+    public String getName();
+    public void setName(String name);
+    public String getSurname();
+    public void setSurname(String surname);
+    public int getYear();
+    public void setYear(int year);
+
+}
---
+++ eJournal/src/Subject.java
@@ -0,0 +1,29 @@
+public class Subject {
+    private String name;
+    private double attendence;
+    private double grade;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getAttendence() {
+        return attendence;
+    }
+
+    public void setAttendence(double attendence) {
+        this.attendence = attendence;
+    }
+
+    public double getGrade() {
+        return grade;
+    }
+
+    public void setGrade(double grade) {
+        this.grade = grade;
+    }
+}
